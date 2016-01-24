766c68ac4b155b33a74eca39b965ab55d5c4873b
Joël Matelli <jo.matelli@gmail.com>
2016-01-24 23:05:38 +0100

    add app.configs.js

--- client/app.js
+++ client/app.js
@@ -3,11 +3,12 @@
 import angular from 'angular';
 import ngMaterial from 'angular-material';
 import ngMdIcons from 'angular-material-icons';
+import ngMessages from 'angular-messages';
 import uiRouter from 'angular-ui-router';
 
 import signUp from './signup';
 import user from './user';
-import routes from './app.routes';
+import {routes, material} from './app.configs';
 
 require('../node_modules/angular-material/angular-material.css');
 require('./app.styl');
@@ -15,23 +16,17 @@ require('./header.styl');
 
 angular
   .module('waisygh', [
+    // angular dependencies
     ngMaterial,
     ngMdIcons,
+    ngMessages,
     uiRouter,
+
+    // app dependencies
     signUp,
     user
   ])
   .constant('API_HOST', '/api/')
-  .config(($mdThemingProvider, $mdIconProvider) => {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('purple')
-      .accentPalette('blue', {
-        'default': '500'
-      })
-    ;
-    $mdIconProvider
-      .defaultIconSet('assets/mdi.svg')
-    ;
-  })
+  .config(material)
   .config(routes)
 ;
